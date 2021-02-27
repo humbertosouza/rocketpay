b4a6a9a51db9f64cd7fabe9f07d69b165cbc4a8c
Humberto Ubuntu <humbertosouza@outlook.com>
2021-02-27 15:10:27 -0500

    Update before refactoring of deposit and withdraw

--- lib/rocketpay/accounts/operation.ex
+++ lib/rocketpay/accounts/operation.ex
@@ -3,12 +3,13 @@ defmodule Rocketpay.Accounts.Operation do
 
   alias Rocketpay.{Account, Repo}
 
-  # read id and value via pattern match
-  def call(%{"id" => id, "value" => value}) do
+  # We do not need to use "run transaction"
+  # Update update_balance to have repo, account, value and operation.
+  def call(%{"id" => id, "value" => value}, operation) do
     Multi.new()
     |>Multi.run(:account, fn repo, _changes -> get_account(repo, id) end)
-    |>Multi.run(:update_balance, fn repo, %{account: account} -> update_balance(repo, account, value) end)
-    |>run_transaction()
+    |>Multi.run(:update_balance, fn repo, %{account: account} ->
+      update_balance(repo, account, value, operation) end)
   end
 
   defp get_account(repo, id) do
@@ -20,28 +21,30 @@ defmodule Rocketpay.Accounts.Operation do
 
   # We need to sum/subtract the value
   # We also need to check if the value is valid
-  defp update_balance(repo, account, value) do
+  defp update_balance(repo, account, value, operation) do
     account
-    |> sum_values(value)
+    |> operation(value, operation)
     |> update_account(repo, account)
   end
 
-  # If you are working with financial data, use the lib decimal.
-  # Phoenix + Ecto make this lib available by default.
-
-  defp sum_values(%Account{balance: balance}, value) do
+  # renamed sum_values to operation as it does more than one operation
+  defp operation(%Account{balance: balance}, value, operation) do
     value
     |> Decimal.cast()
-    |> handle_cast(balance)
+    |> handle_cast(balance, operation)
   end
 
-  defp handle_cast({:ok, value}, balance), do: Decimal.sub(balance, value) #inverted when adding
-  defp handle_cast(:error, _balance), do: {:error, "Invalid deposit value!"}
-
+  #Using patter matching, it will check the third parameter if it is "deposito". If so, add.
+  defp handle_cast({:ok, value}, balance, :deposito), do: Decimal.add(balance, value) #inverted when adding
+  #Using patter matching, it will check the third parameter if it is "withdraw". If so, subtract.
+  defp handle_cast({:ok, value}, balance, :withdraw), do: Decimal.sub(balance, value) #inverted when adding
+  # We need to handle the 3 parameter in the error, ignoring the operation
+  defp handle_cast(:error, _balance, _operation), do: {:error, "Invalid operation value!"}
 
 
   # The last step for updating account
   defp update_account({:error, _reason} = error, _repo, _account), do: error
+
   defp update_account(value, repo, account) do
     params = %{balance: value}
     account
